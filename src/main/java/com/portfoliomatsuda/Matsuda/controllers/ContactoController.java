e00d921b8dca677e3a1785a7d7d2ee64aba2c6ce
Quebin <matsuda.m.kevin@gmail.com>
2022-06-27 14:10:03 -0300

    actualizacion de los metodos http

--- src/main/java/com/portfoliomatsuda/Matsuda/controllers/ContactoController.java
+++ src/main/java/com/portfoliomatsuda/Matsuda/controllers/ContactoController.java
@@ -1,4 +1,40 @@
 package com.portfoliomatsuda.Matsuda.controllers;
 
+import com.portfoliomatsuda.Matsuda.models.Contacto;
+import com.portfoliomatsuda.Matsuda.services.ContactoService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/contacto")
 public class ContactoController {
+
+    private final ContactoService contactoService;
+
+    public ContactoController(ContactoService contactoService) {
+        this.contactoService=contactoService;
+    }
+    @PutMapping("/update")
+    public ResponseEntity<Contacto> editarContacto (@RequestBody Contacto contacto){
+        Contacto updateContacto=contactoService.editarContacto(contacto);
+        return new ResponseEntity<>(updateContacto, HttpStatus.OK);
+    }
+    @GetMapping("/all")
+    public ResponseEntity<List<Contacto>> obtenerContacto(){
+        List<Contacto> contactos=contactoService.buscarContacto();
+        return new ResponseEntity<>(contactos, HttpStatus.OK);
+    }
+    @PostMapping("/add")
+    public ResponseEntity<Contacto> crearContacto(@RequestBody Contacto contacto){
+        Contacto nuevoContacto=contactoService.addContacto(contacto);
+        return new ResponseEntity<>(nuevoContacto, HttpStatus.CREATED);
+    }
+    @DeleteMapping("delete/{id}")
+    public ResponseEntity<?> borrarContacto (@PathVariable("id") Long id){
+    contactoService.borrarContacto(id);
+    return new ResponseEntity<>(HttpStatus.OK);
+    }
 }
--- src/main/java/com/portfoliomatsuda/Matsuda/controllers/ExperienciaController.java
+++ src/main/java/com/portfoliomatsuda/Matsuda/controllers/ExperienciaController.java
@@ -1,4 +1,41 @@
 package com.portfoliomatsuda.Matsuda.controllers;
 
+import com.portfoliomatsuda.Matsuda.models.Experiencia;
+import com.portfoliomatsuda.Matsuda.services.ExperienciaService;
+import org.springframework.data.repository.support.Repositories;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.xml.ws.Response;
+import java.util.List;
+
+@RestController
+@RequestMapping("/experiencia")
 public class ExperienciaController {
+    private final ExperienciaService experienciaService;
+
+    public ExperienciaController(ExperienciaService experienciaService) {
+        this.experienciaService = experienciaService;
+    }
+    @PutMapping("/update")
+    public ResponseEntity<Experiencia> editarExperiencia(@RequestBody Experiencia experiencia){
+        Experiencia updateExperiencia=experienciaService.editarExperiencia(experiencia);
+        return new ResponseEntity<>(updateExperiencia, HttpStatus.OK);
+    }
+    @GetMapping("/all")
+    public ResponseEntity<List<Experiencia>> obtenerExperiencia(){
+        List<Experiencia> experiencias=experienciaService.buscarExperiencia();
+        return new ResponseEntity<>(experiencias, HttpStatus.OK);
+    }
+    @PostMapping("/add")
+    public ResponseEntity<Experiencia> crearExperiencia(@RequestBody Experiencia experiencia){
+        Experiencia nuevaExperiencia=experienciaService.addExperiencia(experiencia);
+        return new ResponseEntity<>(nuevaExperiencia, HttpStatus.CREATED);
+    }
+    @DeleteMapping("delete/{id}")
+    public ResponseEntity<?> borrarExperiencia(@PathVariable("id")Long id){
+        experienciaService.borrarExperiencia(id);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
 }
